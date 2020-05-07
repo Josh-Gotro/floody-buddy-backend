265a3c8f466543019005de4788adc7da20735512
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-07 16:54:38 -0500

    added array of loc names to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,8 @@ User.delete_all
 Gauge.delete_all
 UserGauge.delete_all
 
+locations = ["Barton Ck at SH 71", "Shoal Ck at Silverway Dr", "Waller Ck at Koenig Lane", "Ft Br Boggy Ck at Manor Rd", "Boggy Ck at Webberville Rd", "Walnut Ck at Webberville Rd", "Boggy Ck at Manor Rd", "Shoal Ck at W 12th St", "Waller Ck at Red River St", "Barton Ck abv Barton Spgs", "Barton Ck at Lost Ck Blvd", "Waller Ck at E 1st St", "Colorado Rv at Austin", "W Bouldin Ck at Oltorf Rd", "Barton Ck at Loop 360", "Williamson Ck at Oak Hill", "Slaughter Ck at FM 1826", "Kincheon Br at William Cannon Blvd", "Williamson Ck at Manchaca Rd", "Williamson Ck at Jimmy Clay Rd", "Onion Ck at US Hwy 183", "Dry Ck at Navarro Ck Rd", "Bear Ck bl FM 1826", "Bear Ck at Spillar Ranch Rd", "Slaughter Ck at FM 2304", "Onion Ck at Twin Creeks Rd"]
+
 
 5.times do
     User.create(name: Faker::Internet.unique.username, password: "123" )
@@ -30,3 +32,6 @@ UG_07 = UserGauge.create(user_id: User.second.id, gauge_id: Gauge.second.id)
 UG_08 = UserGauge.create(user_id: User.third.id, gauge_id: Gauge.first.id)
 UG_09 = UserGauge.create(user_id: User.fourth.id, gauge_id: Gauge.fourth.id)
 UG_10 = UserGauge.create(user_id: User.fifth.id, gauge_id: Gauge.fifth.id)
+
+# begin new process
+
