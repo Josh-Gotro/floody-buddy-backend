d964daf0b3d2301bf7cbaa14a64de9fc3b1ce2b7
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-07 22:15:21 -0500

    opened up locations for gauges

--- app/controllers/user_gauges_controller.rb
+++ app/controllers/user_gauges_controller.rb
@@ -13,6 +13,15 @@ class UserGaugesController < ApplicationController
             render json: {message: "sorry friend, no info here"}
         end
     end
+
+    def new
+        user_gauge = UserGauge.new
+    end
+
+    def create 
+        user_gauge = UserGauge.create({gauge_id: params[:gauge_id], user_id: params[:user_id]})
+        render json: user_gauge
+    end
     
     def destroy
         usergauge = UserGauge.find_by({id: params[:id]})   
